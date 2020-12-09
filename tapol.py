0a826bb1ca63e8ed8144b0836a6121586fc943d5
Simon Thompson <simon.thompson@genomicsengland.co.uk>
2020-12-09 19:09:56 +0000

    Hello World working

---
+++ tapol.py
@@ -0,0 +1,8 @@
+from app import app
+import random
+
+if __name__ == "__main__":  # Makes sure this is the main process
+	app.run( # Starts the site
+		host='0.0.0.0',  # EStablishes the host, required for repl to detect the site
+		port=random.randint(2000, 9000)  # Randomly select the port the machine hosts on.
+	)
