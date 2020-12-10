1e253affa8fda43393358266eb6cba07c1bad691
Simon Thompson <simon.thompson@genomicsengland.co.uk>
2020-12-10 20:07:59 +0000

    include templates

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__,
+  template_folder='templates'
+  )
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,19 @@
 from app import app
+from flask import render_template
+
+recordset = [
+  {'id': 1021,
+  'title': 'some stuff',
+  'description': 'some other stuff'},
+  {'id': 1022,
+  'title': 'some different stuff',
+  'description': None},  
+]
 
 @app.route('/')
 @app.route('/index')
 def index():
-  return 'Hello World'
+  return render_template('index.html',
+    title = 'Home',
+    user = {'username': 'Simon'},
+    recordset = recordset)
