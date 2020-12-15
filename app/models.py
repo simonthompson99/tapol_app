caccd98812793be57000337bb0f012c26fd5a8e5
Simon Thompson <simon.thompson@genomicsengland.co.uk>
2020-12-15 19:06:58 +0000

    add edit profile functionality

--- app/models.py
+++ app/models.py
@@ -9,6 +9,8 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    job_title = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
     records = db.relationship('Record', backref = 'author', lazy = 'dynamic')
 
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,9 @@
 from app import app, db
+from datetime import datetime
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
-from app.forms import LoginForm, RegistrationForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm
 from app.models import User
 
 recordset = [
@@ -13,6 +14,12 @@ recordset = [
   'title': 'some different stuff',
   'description': None},  
 ]
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
 
 @app.route('/')
 @app.route('/index')
@@ -71,3 +78,20 @@ def user(username):
         {'author': user, 'body': 'Test post #2'}
     ]
     return render_template('user.html', user=user, records=records)
+
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.job_title = form.job_title.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.job_title.data = current_user.job_title
+    return render_template('edit_profile.html', title='Edit Profile',
+                           form=form)
